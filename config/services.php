656c0ff3f70aee63d2c88e3f310d4de7f2aa1b79
abu baker saddique <abubakar.siddique@miranz.net>
2018-03-09 01:01:01 -0800

    lara-socialite

---
+++ config/services.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+    ],
+
+    'ses' => [
+        'key' => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => 'us-east-1',
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe' => [
+        'model' => App\User::class,
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+
+    'google' => [
+        'client_id'     => '591784055829-6conoch4a2dhraiuegrmrmocg1jh4brq.apps.googleusercontent.com',
+        'client_secret' => 'k56bxAfoLwrJA7QvoWviuaDj',
+        'redirect'      => 'http://127.0.0.1:8000/auth/google/callback',
+    ],
+
+];
